22077c47b5d13708b4f66b6718c90eab1ef3db42
nmcode <nmcode.personal@gmail.com>
2022-04-25 16:26:35 +0300

    test for Vercel

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
+// noinspection ES6CheckImport
+
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import AppHeader from "../appHeader/AppHeader";
-import Spinner from "../spinner/spinner";
+import AppHeader from "./appHeader/AppHeader";
+import Spinner from "./spinner/spinner";
 
-const Page404 = lazy(() => import("../pages/404"));
-const MainPage = lazy(() => import("../pages/MainPage"));
-const ComicsPage = lazy(() => import("../pages/ComicsPage"));
-const SingleComicPage = lazy(() => import("../pages/SingleComicPage"));
+const Page404 = lazy(() => import("./pages/404"));
+const MainPage = lazy(() => import("./pages/MainPage"));
+const ComicsPage = lazy(() => import("./pages/ComicsPage"));
+const SingleComicPage = lazy(() => import("./pages/SingleComicPage"));
 
 const App = () => {
   return (
